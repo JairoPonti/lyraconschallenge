1cbdefb0d96b0828c61982bc4127e7d4f4cf5284
jairoPonti <jairmponti@gmail.com>
2021-06-04 21:59:37 -0300

    Submenú de menú 1(solo animación)

--- lyracons/src/App.js
+++ lyracons/src/App.js
@@ -2,8 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Iniciando</h1>
+    <div >
+      <nav>
+        <ul className="menu">
+      <li id="uno"><a href="#">MENU ITEM 1</a>
+         <ul className="submenu">
+         <li id="uno"><a href="#">SUBMENU ITEM 1</a></li>
+         <li id="uno"><a href="#">SUBMENU ITEM 2</a></li>
+         <li id="uno"><a href="#">SUBMENU ITEM 3</a></li>
+         </ul>
+      </li>
+      <li id="dos">MENU ITEM 2</li>
+      <li id="tres">MENU ITEM 3</li>
+      <li id="cuatro">MENU ITEM 4</li>
+      </ul>
+      </nav>
+    <aside>
+    
+    </aside>
     </div>
   );
 }
