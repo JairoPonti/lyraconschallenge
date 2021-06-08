b63a7f1b9625549b3bceb0344f09b6ae429975dd
jairoPonti <jairmponti@gmail.com>
2021-06-08 18:02:12 -0300

    entrega final

--- lyracons/src/components/Aside.js
+++ lyracons/src/components/Aside.js
@@ -1,26 +1,15 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from '../context/GeneralContext'
-import './aside.css'
-
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/GeneralContext";
+import "./aside.css";
 
 const Aside = () => {
+  const { interruptor, sideColor } = useContext(ThemeContext);
 
-    const { interruptor, sideColor } = useContext(ThemeContext)
-    console.log(sideColor)
- 
-    return (
-      
-        <div className={sideColor}>
-            {interruptor === 'on' ? <p>Hola mundo!</p> : null}
-            {/* <script type="text/javascript">
-            $('#accesoGlobal').change(function() {
-             alert("escuchando")
-    } 
- 
-            </script> */}
-         
-       </div>  
-    )
-}
+  return (
+    <div className={sideColor}>
+      {interruptor === "on" ? <p>Hola mundo!</p> : null}
+    </div>
+  );
+};
 
-export default Aside
+export default Aside;
--- lyracons/src/context/GeneralContext.js
+++ lyracons/src/context/GeneralContext.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
-export const ThemeContext = React.createContext()
+import React, { useState } from "react";
+export const ThemeContext = React.createContext();
 
 const GeneralContext = (props) => {
-    const [interruptor, setInterruptor] = useState('off')
-    const [sideColor, setSidecolor ] = useState('aside')
-    const [interruptorItems, setInterruptorItems] = useState('off')
- console.log(interruptorItems)
-    return (
-        <ThemeContext.Provider value={{setInterruptor, interruptor, sideColor, setSidecolor, setInterruptorItems, interruptorItems}}>
-            {props.children}
-        </ThemeContext.Provider >
-    )
-}
+  const [interruptor, setInterruptor] = useState("off");
+  const [sideColor, setSidecolor] = useState("aside");
+  const [interruptorItems, setInterruptorItems] = useState("off");
 
-export default GeneralContext
+  return (
+    <ThemeContext.Provider
+      value={{
+        setInterruptor,
+        interruptor,
+        sideColor,
+        setSidecolor,
+        setInterruptorItems,
+        interruptorItems,
+      }}
+    >
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default GeneralContext;
